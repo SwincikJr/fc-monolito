94df45d9a54e0bccecf80568902f9b615f1ea9da
SwincikJr <mauroswincikjr@gmail.com>
2025-11-25 19:38:07 -0300

    correção do desafio de invoice

--- src/modules/invoice/domain/invoice.entity.ts
+++ src/modules/invoice/domain/invoice.entity.ts
@@ -2,42 +2,16 @@ import AggregateRoot from "../../@shared/domain/entity/aggregate-root.interface"
 import BaseEntity from "../../@shared/domain/entity/base.entity";
 import Address from "../../@shared/domain/value-object/address";
 import Id from "../../@shared/domain/value-object/id.value-object";
-
-type InvoiceItemInterface = {
-    id?: Id,
-    name: string,
-    price: number,
-    createdAt?: Date,
-    updatedAt?: Date
-}
+import { InvoiceItem, InvoiceItemProps } from "./invoice-item.entity";
 
 type InvoiceProps = {
     id?: Id,
     name: string,
     document: string,
     address: Address,
-    items: InvoiceItemInterface[],
+    items: InvoiceItemProps[],
     createdAt?: Date,
     updatedAt?: Date
-}
-
-export class InvoiceItem extends BaseEntity implements AggregateRoot {
-    private _name: string
-    private _price: number
-
-    constructor(props: InvoiceItemInterface) {
-        super(props.id, props.createdAt, props.updatedAt)
-        this._name = props.name
-        this._price = props.price
-    }
-
-    get name(): string {
-        return this._name
-    }
-
-    get price(): number {
-        return this._price
-    }
 }
 
 export class Invoice extends BaseEntity implements AggregateRoot {
@@ -55,9 +29,7 @@ export class Invoice extends BaseEntity implements AggregateRoot {
         this._items = props.items.map(i => (new InvoiceItem({
             id: i.id,
             name: i.name,
-            price: i.price,
-            createdAt: i.createdAt,
-            updatedAt: i.updatedAt
+            price: i.price
         })))
     }
 
